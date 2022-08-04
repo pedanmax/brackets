b184143689ad42f71ccff0e3868834d2eaf92d13
pedanmax <m.pedan2017@gmail.com>
2022-08-04 15:50:29 +0300

    feat:finished this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let OPEN_BRACKETS = [];
+        let PAIRS_BRACKETS = {};
+        for ( let i = 0; i < bracketsConfig.length; i++){
+            let key = bracketsConfig[i][1];
+            let value = bracketsConfig[i][0];
+            OPEN_BRACKETS.push(bracketsConfig[i][0]);
+            PAIRS_BRACKETS[key] = value;
+        }
+        let stack = [];
+        for (let i = 0; i < str.length; i++){
+            let currentS = str[i];
+            let topElement = stack[stack.length -1]
+                if(stack.length !== 0 && topElement === PAIRS_BRACKETS[currentS]){
+                stack.pop()
+                } else {
+                stack.push(currentS)
+                }
+        }
+        return stack.length === 0;
+    }
